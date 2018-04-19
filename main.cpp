62a5a63cebe6fd2cc9f795bbbd735be4bc48c401
Mahady Hasan <mahadyhasankbc710@gmail.com>
2018-04-19 17:26:48 +0600

    Added a funtion for manual

--- main.cpp
+++ main.cpp
@@ -16,6 +16,30 @@ class todo{
         void manual();
         void check();
 };
+void  todo :: add(){
+        io.open("todo.txt",std::ios::app);
+        t_name();
+        getchar();
+        getline(std::cin,task_name);
+        t_description();
+        getline(std::cin,task_description);
+        t_date();
+        getline(std::cin,task_date);
+        io<<task_name<<std::endl;
+        io<<task_description<<std::endl;
+        io<<task_date<<std::endl;
+        io.close();
+        manual();
+
+}
+void todo :: manual(){
+    std::cout<<"\n\nSelect an option\n";
+    std::cout<<"1.Add\n";
+    std::cout<<"2.Show\n";
+    std::cout<<"3.Delete\n";
+    std::cout<<"4.Exit\n";
+    check();
+}
 int main(){
     todo my;
     my.show();
